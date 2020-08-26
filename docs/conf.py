957515b9cd834df9a9703388058c4d626d36ec38
The Roy Akash <business.theroyakash@outlook.com>
2020-08-26 06:18:29 +0000

    config file changed

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,10 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc']
+<<<<<<< HEAD
 
+=======
+>>>>>>> origin/master
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
