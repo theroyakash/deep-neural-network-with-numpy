c617f65ec551243989c0ae2430357580761abadb
theroyakash <business.theroyakash@outlook.com>
2020-08-26 20:42:56 +0530

    Final Design Update
    Final Design Update

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'alabaster'  # default theme
+html_theme = 'flask'  # Flask style
+# html_theme = 'sphinx_rtd_theme'  # Read the docs style
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
